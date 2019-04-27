1c67aca266ffb106d7454f7baa209db704a6f7f1
Leo Andrés Moreno Caicedo <leo.moreno@innovapps.co>
2019-04-26 23:00:11 -0500

    Se agrega función post

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
@@ -7,7 +8,16 @@ const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 bookRouter.route('/books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+
+    book.save();
+    return res.status(201).json(book);
+  })
   .get((req, res) => {
     const query = {};
     if (req.query.genre) {
@@ -31,6 +41,10 @@ bookRouter.route('/books/:bookId')
     });
   });
 app.use('/api', bookRouter);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my Library API!');
+});
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
