ee7648c1d73e87e5f56d1ca771e1ef229ca1d691
Leo Andres Moreno Caicedo <leo.moreno@innovapps.co>
2019-05-02 23:14:11 -0500

    Version final del backend con el CRUD de books completo

--- app.js
+++ app.js
@@ -3,43 +3,15 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
+
 const db = mongoose.connect('mongodb://localhost/bookAPI');
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-bookRouter.route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
-
-    book.save();
-    return res.status(201).json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-
-bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(book);
-    });
-  });
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
@@ -49,3 +21,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
+
+module.exports = app;
